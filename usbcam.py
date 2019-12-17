ef7f99c0b0e21c54c98285d05ce7d688c883d38d
git <git>
2019-12-17 17:03:59 +0900

    store images

--- usbcam.py
+++ usbcam.py
@@ -1,13 +1,27 @@
 import cv2
 
+count = 0
+max_frame = 100
+
+resolution = (4096,2160)
+
+save_dir = '/Users/test/Desktop/opencv/sample_data/'
 capture = cv2.VideoCapture(1)
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, resolution[0])
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution[1])
 
 while True:
     ret, frame = capture.read()
-    cv2.imshow("VideoFrame", frame)
-    if cv2.waitKey(1) > 0: break
+    # ret => True or False (If cv read frame properly, ret returns true)
+    try:
+        if count < max_frame and ret:
+            cv2.imwrite('{}/{}.jpg'.format(save_dir,count), frame)
+            count += 1
+            print(count)
+    except:
+        pass
+    # cv2.imshow("VideoFrame", frame)
+    # if cv2.waitKey(1) > 0: break
 
 capture.release()
 cv2.destroyAllWindows()
